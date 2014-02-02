442769042a7a3580b72dac347111769ee48b0fab
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2014-02-02 13:53:41 -0700

    added auto-rename.sh

---
+++ auto-rename.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo 'You need to give the first argument as a pattern of files you want to rename.'
+  exit
+fi
+
+if [ -z $2 ]
+then
+  echo 'The second parameter must be the thing you want ot replace in the file name.'
+  echo 'The scrip will replace this part for the third gien parmeter.'
+  echo 'If the third parameter is missing, this part will be deleted from the new file name.'
+  exit
+fi
+
+for i in `ls -1 $1`
+do
+  echo -n "Renaming $i -> "
+  new_name=`echo $i | sed "s/$1/$2/g"`
+  mv $i $new_name
+  echo $new_name
+done
